aa7148f7669f4b4da8347f355ac3770adbb574c3
Shekhar Gulati <shekhargulati84@gmail.com>
2015-12-17 14:32:35 +0530

    Cobertura example

---
+++ core/src/main/java/com/shekhar/ce/core/Calculator.java
@@ -0,0 +1,23 @@
+package com.shekhar.ce.core;
+
+/**
+ * Created by shekhargulati on 17/12/15.
+ */
+public class Calculator {
+
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    public int sub(int a, int b){
+        return a - b;
+    }
+
+    public int mul(int a, int b){
+        return a * b;
+    }
+
+    public int div(int a, int b){
+        return a / b;
+    }
+}
---
+++ web/src/main/java/com/shekhar/ce/web/MyApp.java
@@ -0,0 +1,11 @@
+package com.shekhar.ce.web;
+
+/**
+ * Created by shekhargulati on 17/12/15.
+ */
+public class MyApp {
+
+    public String app() {
+        return "hello";
+    }
+}
